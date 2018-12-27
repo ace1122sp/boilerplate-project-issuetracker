c02501a4ec49aad5d677aff7f0cd4868d48d246e
ace11 <ace11.space22@gmail.com>
2018-12-27 12:13:29 +0100

    project views basic markup completed

--- public/js/project.js
+++ public/js/project.js
@@ -56,6 +56,8 @@
 
       this.renderIssueList();
       this.renderIssueView();
+      this.renderIssueAddForm();
+      this.renderFilterForm();
     },
     renderIssueList: function() {
       const issues = octopus.getIssues();
@@ -77,6 +79,214 @@
     },
     renderIssueView: function() {                
       this.projectHeadline.innerText = this.title.innerText;
+    },
+    renderIssueAddForm: function() {
+      const wrapper = document.createElement('div');
+      wrapper.setAttribute('id', 'add-issue-div');
+
+      const childElements = [];
+
+      const h3 = document.createElement('h3');
+      h3.innerText = 'Add Issue';
+
+      childElements.push(h3);
+
+      const form = document.createElement('form'); // refactor this
+      form.setAttribute('action', '????');
+      form.setAttribute('method', 'POST');
+      form.setAttribute('id', 'add-issue-form');
+
+      // create inner form sections
+      const _createFormSection = param => {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'inner-form-div');
+
+        const label = document.createElement('label');
+        label.setAttribute('for', param.inputId);
+        label.innerText = param.label;
+
+        const input = document.createElement('input');
+        const attributes = [['type', 'text'], ['name', param.inputName], ['id', param.inputId], ['required', param.required]];
+        attributes.forEach(attr => {
+          if (attr[0] === 'required' && attr[1] === false) return;
+          input.setAttribute(attr[0], attr[1]);
+        });      
+
+        [label, input].forEach(elm => {
+          div.appendChild(elm);
+        });
+
+        return div;
+      }
+
+      const formElements = [
+        {
+          inputId: 'add-issue-form-title',
+          inputName: 'issue_title',
+          label: 'issue title: ',
+          required: true
+        },
+        {
+          inputId: 'add-issue-form-text',
+          inputName: 'issue_text',
+          label: 'issue text: ',
+          required: true
+        },
+        {
+          inputId: 'add-issue-form-created-by',
+          inputName: 'created_by',
+          label: 'created by: ',
+          required: true
+        },
+        {
+          inputId: 'add-issue-form-assigned-to',
+          inputName: 'assigned_to',
+          label: 'assigned to: ',
+          required: false
+        },
+        {
+          inputId: 'add-issue-form-status-text',
+          inputName: 'status_text',
+          label: 'status text: ',
+          required: false
+        },
+      ];
+
+      formElements.forEach(elm => {
+        let element = _createFormSection(elm);
+        form.appendChild(element);
+      });
+
+      const _generateControlButtons = btn => {
+        const { attr, attrVal, innerText } = btn;
+        const button = document.createElement('button');
+        button.setAttribute(attr, attrVal);
+        button.innerText = innerText;
+
+        return button;
+      }
+      // add submit button to form
+      const submitBtn = _generateControlButtons({ attr: 'type', attrVal: 'submit', innerText: 'add' });
+      form.appendChild(submitBtn);
+
+      childElements.push(form);
+
+      // cancel and close form
+      const cancelBtn = _generateControlButtons({ attr: 'id', attrVal: 'cancel-adding-issue-btn', innerText: 'cancel' });    
+      childElements.push(cancelBtn);
+
+      // append child elements to wrapper
+      childElements.forEach(elm => {
+        wrapper.appendChild(elm);
+      });
+
+      // render 
+      this.main.appendChild(wrapper);
+    },
+    renderFilterForm: function() {
+      const wrapper = document.createElement('div');
+      wrapper.setAttribute('id', 'filter-issues-div');
+
+      const childElements = [];
+
+      const h3 = document.createElement('h3');
+      h3.innerText = 'Filter Issues';
+      
+      childElements.push(h3);
+
+      // add input elements
+      const labels = [
+        {
+          inputId: 'filter-title',
+          inputName: 'issue_title',
+          label: 'issue title: '
+        },
+        {
+          inputId: 'filter-text',
+          inputName: 'issue_text',
+          label: 'issue text: '
+        },
+        {
+          inputId: 'filter-created-by',
+          inputName: 'created_by',
+          label: 'created by: '
+        },
+        {
+          inputId: 'filter-assigned-to',
+          inputName: 'assigned_to',
+          label: 'assigned to: '
+        },
+        {
+          inputId: 'filter-status-text',
+          inputName: 'status_text',
+          label: 'status text: '
+        },
+        {
+          inputId: 'filter-created-on',
+          inputName: 'created_on',
+          label: 'created on: '
+        },
+        {
+          inputId: 'filter-updated-on',
+          inputName: 'updated_on',
+          label: 'updated on: '
+        },
+        {
+          inputId: 'open',
+          inputName: 'open',
+          label: 'open: '
+        }
+      ];
+      
+      const _generateInputDivs = input => {
+        const { inputId, inputName, label } = input;
+        
+        const div = document.createElement('div');
+        
+        const labelElm = document.createElement('label');
+        labelElm.setAttribute('for', inputId);
+        labelElm.innerText = label;
+
+        div.appendChild(labelElm);
+
+        const inputElm = document.createElement('input');
+        const inputAttrs = [['type', 'text'], ['id', inputId], ['name', inputName]];
+        inputAttrs.forEach(attr => {
+          inputElm.setAttribute(attr[0], attr[1]);
+        });
+
+        div.appendChild(inputElm);
+
+        return div
+      }
+
+      labels.forEach(l => {
+        const inputDiv = _generateInputDivs(l);
+        childElements.push(inputDiv);
+      });
+
+      // add control buttons
+      const buttons = ['ok', 'cancel'];
+
+      const _generateControlButtons = val => {
+        const btn = document.createElement('button');
+        btn.innerText = val;
+
+        return btn;
+      }
+
+      buttons.forEach(b => {
+        let btn = _generateControlButtons(b);
+        childElements.push(btn);
+      });
+
+      // append child elements to wrapper
+      childElements.forEach(elm => {
+        wrapper.appendChild(elm);
+      });
+
+      // render filter form
+      this.main.appendChild(wrapper);
     },
     renderIssueCard: function(issueId) {      
       const issue = octopus.getIssue(issueId);
